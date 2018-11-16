a796069ee33e7244371688786466cb47df5b11fb
tiffanymugure <amiratiffany@gmail.com>
2018-11-16 15:52:30 +0300

    Add Config,ProdConfig & DevConfig classes and config_options dictionary

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
