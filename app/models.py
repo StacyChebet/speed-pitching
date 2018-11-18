25c23483de6283ee589016fce3ebff8e35b6332c
StacyChebet <amiratiffany@gmail.com>
2018-11-18 20:27:48 +0300

    create relationship between users and pitches tables

--- app/models.py
+++ app/models.py
@@ -7,9 +7,9 @@ class User(db.Model):
     email = db.Column(db.String)
     profile = db.Column(db.String)
     prof_pic = db.Column(db.String)
+    pitch = db.relationship("Pitch", backref="user", lazy="dynamic")
+    Comment = db.relationship("Comment", backref ="user", lazy="dynamic")
 
-    def __repr__(self):
-        return f'User {self.username}'
    
 
 class Pitch:
@@ -18,11 +18,15 @@ class Pitch:
     '''
     __tablename__ = "pitches"
     id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    name = db.Column(db.String)
     category = db.Column(db.String)
     pitch = db.Column(db.string(255))
     upvotes = db.Column(db.Integer)
     downvotes = db.Column(db.Integer)
     
+    def __repr__(self):
+        return f'User {self.username}'
 
     
 
