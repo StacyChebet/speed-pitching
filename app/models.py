46d9201497bb25b6192d4cace353f7c91c49569b
StacyChebet <amiratiffany@gmail.com>
2018-11-18 20:01:08 +0300

    add the Pitches class

--- app/models.py
+++ app/models.py
@@ -4,19 +4,26 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String)
+    profile = db.Column(db.String)
+    prof_pic = db.Column(db.String)
 
     def __repr__(self):
         return f'User {self.username}'
    
 
-class Pitches:
+class Pitch:
     '''
     User class to define pitch objects
     '''
-    def __init__(self,pitch,category,upvote,downvote):
-        self.pitch = pitch
-        self.category = category
-        self.upvote = upvote
-        self.downvote = downvote
+    __tablename__ = "pitches"
+    id = db.Column(db.Integer, primary_key = True)
+    category = db.Column(db.String)
+    pitch = db.Column(db.string(255))
+    upvotes = db.Column(db.Integer)
+    downvotes = db.Column(db.Integer)
+    
+
+    
 
 
