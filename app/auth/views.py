cbbc4eb880e03d4ff9ae7b937564c6276fb2a14f
StacyChebet <staceychebet23@gmail.com>
2018-11-19 19:27:28 +0300

    add auth route logout to logout user from application

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template,redirect,url_for,flash,request
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from . import auth
 from ..models import User,Pitch,Comment
 from .forms import LoginForm,SignupForm
@@ -29,4 +29,9 @@ def login():
     
     title = "Speed Pitching Login"
     return render_template('auth/login.html', login_form = login_form, title = title)
-    return render_template('auth/login.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
