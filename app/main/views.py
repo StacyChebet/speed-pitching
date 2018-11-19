0fffb18b1e84060e030272f4b98feae7e7d31381
StacyChebet <staceychebet23@gmail.com>
2018-11-19 22:46:01 +0300

    add view function for user profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for,abort
+from ..models import User,Pitch,Comment
 from . import main
+from flask_login import login_required
 
 #views
 @main.route('/')
@@ -7,4 +9,14 @@ def index():
     '''
     Returns index page and it's data
     '''
+    title = "Speed Pitching"
     return render_template('index.html', title = title)
+
+@main.route('/user/<username>')
+def profile(username):
+    user = User.query.filter_by(username = username).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
