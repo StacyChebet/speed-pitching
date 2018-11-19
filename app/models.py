c5961cdd022f7c099b982455bf3a654e3aa1a66d
StacyChebet <staceychebet23@gmail.com>
2018-11-19 10:47:58 +0300

    Add load_user function to query db and get user by id

--- app/models.py
+++ app/models.py
@@ -1,24 +1,29 @@
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(255))
-    email = db.Column(db.String)
+    username = db.Column(db.String(255), index = True)
+    email = db.Column(db.String(255), unique = True, index = True)
     profile = db.Column(db.String)
     prof_pic = db.Column(db.String)
     pitch = db.relationship("Pitch", backref="user", lazy="dynamic")
     Comment = db.relationship("Comment", backref ="user", lazy="dynamic")
-    pass_secure = db.Column(db.String(255))
+    pass_hash = db.Column(db.String(255))
 
     @property
     def password(self):
         raise AttributeError('Stick to your lane bro!')
     
     def verify_password(self, password):
-        return check_password_hash(self.pass_secure, password)
+        return check_password_hash(self.pass_hash, password)
 
 class Pitch(db.Model):
     '''
