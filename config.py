40d5d13811b005aa915d01ce54bc986c8805a26f
StacyChebet <staceychebet23@gmail.com>
2018-11-21 09:58:54 +0300

    add the email configurations

--- config.py
+++ config.py
@@ -2,7 +2,16 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://stacy:stacy@localhost/pitches"
+    UPLOADED_PHOTOS_DEST = "app/static/photos"
 
+    #Email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
 class ProdConfig(Config):
     pass
 
