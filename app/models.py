a2fef8e9a2de9b33e8d087fe9b2d5e240ccd7b4d
StacyChebet <staceychebet23@gmail.com>
2018-11-22 06:31:29 +0300

    Add variables for date and time to enable sorting in descending order

--- app/models.py
+++ app/models.py
@@ -13,17 +13,33 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255), index = True)
     email = db.Column(db.String(255), unique = True, index = True)
     profile = db.Column(db.String)
-    prof_pic = db.Column(db.String)
+    prof_pic = db.Column(db.String())
     pitch = db.relationship("Pitch", backref="user", lazy="dynamic")
-    Comment = db.relationship("Comment", backref ="user", lazy="dynamic")
+    comment = db.relationship("Comment", backref ="user", lazy="dynamic")
     pass_hash = db.Column(db.String(255))
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
 
     @property
     def password(self):
-        raise AttributeError('Stick to your lane bro!')
+        raise AttributeError('Stick to your lane!')
     
+    @password.setter
+    def password(self,password):
+        self.pass_hash = generate_password_hash(password)
+
     def verify_password(self, password):
         return check_password_hash(self.pass_hash, password)
+
+    def get_pitches(self):
+        user = User.query.filter_by(id = self.id).first()
+        return user.pitch
+    
+    def get_comments(self):
+        user = User.query.filter_by(id = self.id).first()
+        return user.comment
 
 class Pitch(db.Model):
     '''
@@ -38,6 +54,16 @@ class Pitch(db.Model):
     upvotes = db.Column(db.Integer)
     downvotes = db.Column(db.Integer)
     comments = db.relationship("Comment", backref="pitch", lazy="dynamic")
+    date_created = db.Column(db.String)
+    time_created = db.Column(db.String)
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def pitch_comments(self):
+        pitch = Pitch.query.filter_by(id = self.id).first()
+        comments = Comment.query.filter_by(pitch_id = pitch.id).order_by(Comment.time_created.desc())
 
 class Comment(db.Model):
     '''
@@ -48,11 +74,9 @@ class Comment(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     pitch_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
     comment = db.Column(db.String(255))
+    date_created = db.Column(db.String)
+    time_created = db.Column(db.String)
 
-    
-    def __repr__(self):
-        return f'User {self.username}'
-
-    
-
-
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
